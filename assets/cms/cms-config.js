e3e106ed46aa34ec9a19ecd280aeaee99d4b84a7
LeNguyenAI <160442037+LeNguyenAI@users.noreply.github.com>
2026-07-11 16:41:20 +0700

    update web

--- assets/cms/cms-config.js
+++
@@ -1,5 +0,0 @@
-window.BEN_CHILL_CMS_CONFIG = {
-  supabaseUrl: "https://pcijlqfnxxlyrcftgkoh.supabase.co",
-  supabaseAnonKey: "sb_publishable_DSxEKcRdGYs35QXjhPB79Q_icToJZCW",
-  storageBucket: "ben-chill-media"
-};
